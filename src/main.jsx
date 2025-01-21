2763d1e8db5f2ee0eeee4a098ebd435349b8e98c
ayubkhan-x <khakimovayubkhan@gmail.com>
2025-01-21 20:32:48 +0500

    Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
 import 'macro-css';
 
-import App from './App.jsx'
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
